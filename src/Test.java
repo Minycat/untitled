f9ba2a7deaa289f3f3f25fb38c8fa12a2b9dc425
student <josiaht23@nycstudents.net>
2023-09-19 12:36:35 -0400

    added a constructor, some more methods, and made the code function for the first time!

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,8 @@
 public class Test {
     public static void main(String[] args) {
+        //Starts the user with 72 stones
         Stone s = new Stone(72);
+        //Tells the user how many stones they have now
         s.stoneCheck();
         s.grabStone();
         s.buildWall();
