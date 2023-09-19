2adcb10097ffb774ba191f65082830a514bbea73
student <josiaht23@nycstudents.net>
2023-09-19 12:34:32 -0400

    added a constructor, some more methods, and made the code function for the first time!

--- src/Stone.java
+++ src/Stone.java
@@ -9,15 +9,32 @@ public class Stone {
     Scanner ask = new Scanner(System.in);
 
     //methods
-    public void stoneCheck(int s) {
-        System.out.println("You are holding" + numStones + " stones now.");
+    public Stone(int n) {
+        numStones = n;
     }
+    public void stoneCheck()
+        {
+        System.out.println("You are holding " + numStones + " stones now.");
+        }
+    public void wallCheck()
+        {
+        System.out.println("The wall is made of " + wall + " stones now.");
+        }
 
-    public void grabStone() {
+    public void grabStone()
+        {
         System.out.println("How many stones do you want to grab?");
         int grab = ask.nextInt();
         numStones += grab;
-        stoneCheck(numStones);
+        stoneCheck();
 
-    }
+        }
+    public void buildWall()
+        {
+        wall += numStones;
+        System.out.println("You add " + numStones + " stones to the wall.");
+        numStones = 0;
+        wallCheck();
+
+        }
 }
--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,9 @@
 public class Test {
     public static void main(String[] args) {
+        Stone s = new Stone(72);
+        s.stoneCheck();
+        s.grabStone();
+        s.buildWall();
 
     }
 }
